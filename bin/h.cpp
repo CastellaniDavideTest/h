60aefd653937d0e0a2a0fc3b46e4930deb3b73e3
Davide Castellani <38205382+DavideC03@users.noreply.github.com>
2020-11-29 11:11:39 +0100

    Created bin/h.cpp

---
+++ bin/h.cpp
@@ -0,0 +1,42 @@
+/**
+ * @file h.cpp
+ *
+ * @version 01.01 20201129
+ *
+ * @brief 
+ *
+ * @ingroup h
+ * (Note: this needs exactly one @defgroup somewhere)
+ *
+ * @author Castellani Davide
+ *
+ * Contact: contacts@castellanidavide.it
+ *
+ */
+
+// Includes
+#include <bits/stdc++.h>
+using namespace std;
+
+// Variabiles
+int N;
+
+// Main code
+int main()
+{
+  // Cncomment the following lines if you want to read/write from files
+  // freopen("input.txt", "r", stdin);
+  // freopen("output.txt", "w", stdout);
+
+  // Input
+  cin >> N;
+
+  // Code
+  // ...
+
+  // Output
+  cout << N << endl;
+
+  // End
+  return 0;
+}
